416a6577db4b2f66efc7fa4e6939c14dedc7400f
dev-gaitano <dev.gaitano@gmail.com>
2025-02-05 21:12:54 +0300

    Defined the barebones workings of the html script

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Listen for user completes order
+
+// Dislay payment pop-up
+
+// Process and confirm the payment
+
+// Submit new invoice data
+
+// Get inputs from new invoice recorded
+
+// Add inputs to invoice list array, to their respective indices
+
+// Deduct quantity of items sold from inventory array
+
+// Add data to respective sections in the dashboard
