bfe75e0337e1032ea79a6882ac2cad876aa70007
dev-gaitano <dev.gaitano@gmail.com>
2025-02-06 10:12:38 +0300

    Added get client info logic

--- js/script.js
+++ js/script.js
@@ -7,7 +7,23 @@
 // Submit new invoice data
 
 // Get inputs from new invoice recorded
+const getClientInfo = () => {
+  const clientName = document.querySelector("#client_name").value;
+  const clientOrganization = document.querySelector("#client_org").value;
+  const clientPhone = document.querySelector("#client_tel").value;
+  const clientEmail = document.querySelector("#client_email").value;
+  const clientAddress = document.querySelector("#client_address").value;
+  const shipTo = document.querySelector("#ship_to").value;
 
+  console.log(
+    clientName,
+    clientOrganization,
+    clientPhone,
+    clientEmail,
+    clientAddress,
+    shipTo
+  );
+};
 // Add inputs to invoice list array, to their respective indices
 
 // Deduct quantity of items sold from inventory array
